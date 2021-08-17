a1dfed81d701c63e08a764d303a0eb8c9167d17f
Guilherme Oliveira <dev.guilherme_oliveira@outlook.com>
2021-08-17 13:08:30 -0300

    refactor: added content to html file, then js only get the element from the html file

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -1,6 +1,3 @@
 import './style.css'
 
-document.querySelector('[data-js="app"]').innerHTML = `
-  <h1>B. Academy</h1>
-  <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
-`
+const app = document.querySelector('[data-js="app"]');
