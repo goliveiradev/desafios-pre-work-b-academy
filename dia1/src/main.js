dc675104ff707df16d7fd476eba5c28a54e873d9
Guilherme Oliveira <dev.guilherme_oliveira@outlook.com>
2021-08-17 13:09:07 -0300

    feat: added a link/button to alter the app element visibility

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -1,3 +1,11 @@
 import './style.css'
 
 const app = document.querySelector('[data-js="app"]');
+
+const link = document.querySelector('[data-js="link"]');
+link.addEventListener('click', (event) => {
+  if (app.style.visibility === "hidden")
+    app.style.visibility = "visible";
+  else
+    app.style.visibility = "hidden";
+}, false);
