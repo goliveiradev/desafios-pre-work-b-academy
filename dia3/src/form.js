b6e715bc85a4dda0f7325a7d921ac2bb551a0ee2
Guilherme Oliveira <dev.guilherme_oliveira@outlook.com>
2021-08-21 18:29:59 -0300

    feat: added a color select

--- dia3/src/form.js
+++ dia3/src/form.js
@@ -16,3 +16,32 @@ input.addEventListener('input', (event) => {
 function capitalize(word) {
   return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
 }
+
+const form = document.querySelector('[data-js="form"]')
+const select = document.createElement('select')
+const colors = ['#6B7280', '#EF4444', '#F59E0B', '#10B981', '#3B82F6']
+const colorsContainer = document.createElement('div')
+colorsContainer.style.display = 'flex'
+
+colors.forEach(color => {
+  const option = document.createElement('option')
+  option.value = color
+  option.textContent = color
+  select.appendChild(option)
+})
+
+select.addEventListener('change', (event) => {
+  colorsContainer.innerHTML = ''
+
+  Array.from(event.target.selectedOptions).forEach(option => {
+    const div = document.createElement('div')
+    div.style.height = '100px'
+    div.style.width = '100px'
+    div.style.background = option.value
+    colorsContainer.appendChild(div);
+  })
+})
+
+select.setAttribute('multiple', '')
+form.appendChild(select)
+document.body.appendChild(colorsContainer)
